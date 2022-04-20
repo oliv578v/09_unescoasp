9fc573e559aa29e7a375cd82fd7bc4d4a4de7461
Your Name <you@example.com>
2022-04-20 20:48:26 +0200

    omunesco

--- wp-content/themes/ourtheme/page-om-unesco-asp.php
+++ wp-content/themes/ourtheme/page-om-unesco-asp.php
@@ -27,10 +27,31 @@ get_header();
 
 	?>
 <style>
-    section.elementor-section:nth-child(1) {
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+    /* .elementor-column-gap-default > .elementor-column > .elementor-element-populated {
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr 1fr;
+} */
+
+    .elementor-element-3558f3e > div:nth-child(1) {
+grid-column: 1/3;
+grid-row: 1;
+}
+
+.elementor-element-248486c > div:nth-child(1) > p:nth-child(2) {
+	grid-column: 1/3;
+	grid-row: 1;
+}
+
+.elementor-element-85566b3 {
+	grid-row: 2;
+	grid-column: 1;
 }
 </style>
 
